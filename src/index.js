4e8380f8fa210f1d1d8da6d761a6407328d01511
tkddbs1008 <finibin08@gmail.com>
2022-04-08 16:04:23 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './shared/App'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import store from "./redux/configureStore"
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+  </Provider>
+  ,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/configureStore.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+});
+
+const middlewares = [thunk];
+
+// 개발환경에서는 로거라는 걸 하나만 더 써볼게요.
+const env = process.env.NODE_ENV;
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middlewares)
+);
+
+let store = (initialStore) => createStore(rootReducer, enhancer);
+
+export default store();
