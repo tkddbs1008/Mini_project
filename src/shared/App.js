050a346a39f008ac01c3146601e663edbd43a443
moomin97 <moomin9997@gmail.com>
2022-04-08 18:33:29 +0900

    mini

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {Route, Routes} from 'react-router-dom'
+import login from "../pages/login";
+import Posts from "../pages/Posts";
+import Signup from "../pages/signup";
+
+
+
 
 function App() {
   return (
     <div >
-
+      <Routes>
+        <Route path="/" element={<Posts/>}/>
+        <Route path="/login/*" element={<login/>}/>
+        <Route path="/signup/*" element={<Signup/>}/>
+      </Routes>
     </div>
   );
 }
