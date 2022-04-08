85e32b2c36782167ef704f03f18568026166eb14
tkddbs1008 <finibin08@gmail.com>
2022-04-08 18:47:07 +0900

    simple header

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,11 +3,13 @@ import {Route, Routes} from 'react-router-dom'
 import Login from "../pages/login";
 import Posts from "../pages/Posts";
 import Signup from "../pages/signup";
+import Header from "./header";
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Routes>
         <Route path="/" element={<Posts/>}/>
         <Route path="/login/*" element={<Login/>}/>
--- src/shared/header.js
+++ src/shared/header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import {useNavigate} from 'react-router-dom'
 
-const header = (props) => {
-    
+
+const Header = (props) => {
+    const nav = useNavigate();
+
+    return (
+        <div>
+            <Stack direction="row" spacing={2} >
+                <Button onClick={()=> nav('/login')} >로그인</Button>
+                <Button onClick={()=> nav('/signup')}>회원가입</Button>
+            </Stack>
+        </div>
+    )
 }
 
-export default header;
+export default Header;
